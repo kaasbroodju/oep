002923e2b481c714184eeb26adc050beb26fd031
kaasbroodju <morriswaaijer8@gmail.com>
2021-11-18 15:59:00 +0100

    random package import gone

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import challenge2.Main;
 import com.example.demo.modloader.ModLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
